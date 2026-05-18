8364e0d3e8c6b7d519ca6c5039313bc88a6a5ccb
Ajayi Olamide <prodbyola@gmail.com>
2026-05-18 09:42:17 +0100

    fix: calling qb.pool() instead qb.pool in models

--- src/model.rs
+++ src/model.rs
@@ -9,20 +9,20 @@ pub trait Model: Sized + Send + Unpin + for<'r> FromRow<'r, <QbEngine as Databas
     fn insert<'q>(qb: &'q QB<'q, Self>) -> impl Future<Output = Result<Self::InsertReturns, sqlx::Error>>;
 
     fn select<'q>(qb: &'q QB<'q, Self>) -> impl Future<Output = Result<Self, sqlx::Error>> {
-        async { qb.fetch_one(qb.pool).await }
+        async { qb.fetch_one(qb.pool()).await }
     }
 
     fn select_all<'q>(
         qb: &'q QB<'q, Self>,
     ) -> impl Future<Output = Result<Vec<Self>, sqlx::Error>> {
-        async { qb.fetch_all(qb.pool).await }
+        async { qb.fetch_all(qb.pool()).await }
     }
 
     fn select_fields<'q, R>(qb: &'q QB<'q, Self>) -> impl Future<Output = Result<R, sqlx::Error>>
     where
         R: Send + Unpin + for<'r> FromRow<'r, <QbEngine as Database>::Row>,
     {
-        async { qb.fetch_fields_one(qb.pool).await }
+        async { qb.fetch_fields_one(qb.pool()).await }
     }
 
     fn select_fields_all<'q, R>(
@@ -31,15 +31,15 @@ pub trait Model: Sized + Send + Unpin + for<'r> FromRow<'r, <QbEngine as Databas
     where
         R: Send + Unpin + for<'r> FromRow<'r, <QbEngine as Database>::Row>,
     {
-        async { qb.fetch_fields_all(qb.pool).await }
+        async { qb.fetch_fields_all(qb.pool()).await }
     }
 
     fn update<'q>(qb: &'q QB<'q, Self>) -> impl Future<Output = Result<QbResult, sqlx::Error>> {
-        async { qb.execute(qb.pool).await }
+        async { qb.execute(qb.pool()).await }
     }
 
     fn delete<'q>(qb: &'q QB<'q, Self>) -> impl Future<Output = Result<QbResult, sqlx::Error>> {
-        async { qb.execute(qb.pool).await }
+        async { qb.execute(qb.pool()).await }
     }
 }
 
